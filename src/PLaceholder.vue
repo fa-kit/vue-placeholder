d2db2f63d0c20fcd0625ee8e765ccdff9d4eeaa7
Arseny <arseny@i-am.studio>
2020-06-08 01:25:02 +0300

    Initial commit

---
+++ src/PLaceholder.vue
@@ -0,0 +1,38 @@
+<template>
+    <div 
+        :style="styles"
+        class="placeholder">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        display: {
+            type: String,
+            default: 'block'
+        },
+        maxWidth: {
+            type: String,
+            default: '100%'
+        },
+        paddingBottom: String,
+        marginBottom: String,
+        backgroundColor: {
+            type: String,
+            default: '#FAFAFA'
+        }
+    },
+    computed: {
+        styles() {
+            return{
+                display: this.display,
+                maxWidth: this.maxWidth,
+                paddingBottom: this.paddingBottom,
+                marginBottom: this.marginBottom,
+                backgroundColor: this.backgroundColor,
+            }
+        },
+    }
+}
+</script>
